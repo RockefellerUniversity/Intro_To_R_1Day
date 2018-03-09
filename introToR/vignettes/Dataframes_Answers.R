84f9d7d531c94b3b240b26b13e63bc3dd9f3165f
Thomas Carroll <tc.infomatics@gmail.com>
2018-03-08 20:40:28 -0500

    .

---
+++ introToR/vignettes/Dataframes_Answers.R
@@ -0,0 +1,34 @@
+## ----setup, include=FALSE------------------------------------------------
+knitr::opts_chunk$set(echo = T,eval = T)
+
+## ----message=T-----------------------------------------------------------
+Annotation <- data.frame(geneNames=c("Gene_1", "Gene_2", "Gene_3","Gene_4","Gene_5"), ensembl=c("Ens001", "Ens003", "Ens006", "Ens007", "Ens010"),pathway=c("Glycolysis", "TGFb", "Glycolysis", "TGFb", "Glycolysis"),geneLengths=c(100, 3000, 200, 1000,1200))
+
+## ----message=T-----------------------------------------------------------
+Sample1 <- data.frame(ensembl=c("Ens001", "Ens003", "Ens006","Ens010"),expression=c(1000, 3000, 10000,5000))
+
+## ----message=T-----------------------------------------------------------
+Sample2 <- data.frame(ensembl=c("Ens001", "Ens003", "Ens006","Ens007","Ens010"),expression=c(1500, 1500, 17000,500,10000))
+
+## ----message=T-----------------------------------------------------------
+AnnoSample1 <- merge(Annotation,Sample1,by.x=2,by.y=1,all=F) 
+AnnoSample1And2 <- merge(AnnoSample1,Sample2,by=1,all=F) 
+AnnoSample1And2
+
+## ----message=T-----------------------------------------------------------
+AnnoSample1And2$Sample1_lne <- AnnoSample1And2$expression.x/AnnoSample1And2$geneLengths
+AnnoSample1And2$Sample2_lne <- AnnoSample1And2$expression.y/AnnoSample1And2$geneLengths
+AnnoSample1And2
+
+## ----message=T-----------------------------------------------------------
+rownames(AnnoSample1And2) <- AnnoSample1And2$ensembl
+mean(AnnoSample1And2["Ens006","Sample1_lne"],AnnoSample1And2["Ens006","Sample2_lne"])
+
+## ----message=T-----------------------------------------------------------
+log2FoldChange <- log2(AnnoSample1And2$Sample2_lne) - log2(AnnoSample1And2$Sample1_lne)
+names(log2FoldChange) <- AnnoSample1And2$geneNames
+log2FoldChange
+
+## ----message=T-----------------------------------------------------------
+sum(AnnoSample1And2[AnnoSample1And2$pathway == "Glycolysis","geneLengths"])
+
---
+++ introToR/vignettes/conditionsAndLoops_Answers.R
@@ -0,0 +1,71 @@
+## ----setup, include=FALSE------------------------------------------------
+knitr::opts_chunk$set(echo = T,eval = T)
+
+## ----message=T-----------------------------------------------------------
+
+for(x in 1:10){
+  if(x == 1){
+    factorialAnswer <- 1
+  }else{
+    factorialAnswer <- factorialAnswer * x 
+  }
+}
+factorialAnswer
+
+## ----message=T-----------------------------------------------------------
+
+factorialAnswer <- 0
+count <- 0
+
+while(factorialAnswer <= 1000){
+  count <- count+1
+  if(count == 1){
+    factorialAnswer <- 1
+  }else{
+    factorialAnswer <- factorialAnswer * count 
+  }
+}
+count
+
+## ----message=T-----------------------------------------------------------
+condExercise <- 1:40
+condExercise
+vectorResult <- ifelse(condExercise<10,"small",ifelse(condExercise < 31,"mid","big"))
+temp <- factor(vectorResult,levels=c("small","mid","big"),order=T)
+temp
+
+## ----message=T-----------------------------------------------------------
+exprDirectory <- system.file("extdata", "ExpressionResults/", package = "introToR")
+
+filesToRead <- dir(exprDirectory,pattern = "*\\.txt",full.names=T)
+fileRead <- vector("list",length=length(filesToRead))
+for(i in 1:length(filesToRead)){
+  fileRead[[i]] <- read.delim(filesToRead[i],header=F,sep="\t")
+  colnames(fileRead[[i]]) <- c("GeneNames",basename(filesToRead[i]))
+}
+mergedTable <- NULL
+for(i in fileRead){
+  if(is.null(mergedTable)){
+    mergedTable <- i
+  }else{
+    mergedTable <- merge(mergedTable,i,by=1,all=T)
+  }
+  
+  print(nrow(mergedTable))
+}
+
+mergedTable[1:3,] 
+
+
+## ----message=T-----------------------------------------------------------
+#Annotation <- read.table("../ExpressionResults/Annotation.ann",sep="\t",h=T)
+annFile <- system.file("extdata", "ExpressionResults/Annotation.ann", package = "introToR")
+
+Annotation <- read.table(annFile,sep="\t",h=T)
+
+annotatedExpression <- merge(Annotation,mergedTable,by=1,all.x=F,all.y=T)
+annotatedExpression[1:2,]
+
+summary(annotatedExpression$Pathway)
+summary(Annotation$Pathway)
+
