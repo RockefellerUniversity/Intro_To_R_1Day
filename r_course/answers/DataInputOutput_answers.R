3d2d484a39ccbdae38bb15434311de2b9db463f3
matthew-paul-2006 <matthew.paul.2006@gmail.com>
2020-04-02 16:38:05 -0400

    recompile everything after updates

--- r_course/answers/DataInputOutput_answers.R
+++ r_course/answers/DataInputOutput_answers.R
@@ -1,8 +1,8 @@
-## ----message=T-----------------------------------------------------------------------
+## ----message=T--------------------------------------------------------------------------
 geneExpression <- read.table("data/GeneExpression.txt",h=T,sep="\t",row.names=1)
 
 
-## ----message=T-----------------------------------------------------------------------
+## ----message=T--------------------------------------------------------------------------
 sampleMeans <- c(mean(geneExpression[,1]),mean(geneExpression[,2]),mean(geneExpression[,3]),mean(geneExpression[,4]),mean(geneExpression[,5]),mean(geneExpression[,5]))
 names(sampleMeans) <- colnames(geneExpression)
 sampleMeans
@@ -10,17 +10,17 @@ sampleMeans
 colMeans(geneExpression)
 
 
-## ----message=T-----------------------------------------------------------------------
+## ----message=T--------------------------------------------------------------------------
 geneExpression <- read.table("data/GeneExpressionWithMethods.txt",h=T,sep="\t",row.names=1,skip=3)
 geneExpression
 
 
-## ----message=T-----------------------------------------------------------------------
+## ----message=T--------------------------------------------------------------------------
 geneExpression <- read.table("data/GeneExpressionWithNotes.txt",h=T,sep="\t",row.names=1,comment.char = ">")
 geneExpression
 
 
-## ----message=T-----------------------------------------------------------------------
+## ----message=T--------------------------------------------------------------------------
 geneExpression <- read.table("data/GeneExpression.txt",h=T,sep="\t",row.names=1)
 orderedExpression <- geneExpression[order(geneExpression$Sample_1.hi,decreasing=T),]
 expressionDF <- cbind(rownames(orderedExpression),orderedExpression)
