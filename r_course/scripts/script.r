652703fd9030946a24f3c863f0cc3d9ef2f7bea9
root <tc.infomatics@gmail.com>
2020-05-30 14:08:08 +0000

    Autobuild

---
+++ r_course/scripts/script.r
@@ -0,0 +1,10 @@
+sample1 <- rnorm(100,100,20)
+sample2 <- rnorm(100,200,20)
+hist(sample1)
+hist(sample2)
+res <- t.test(sample1,sample2)
+res
+boxplot(sample1,sample2,col=c("red","green"))
+summary(sample1)
+summary(sample2)
+sessionInfo()
